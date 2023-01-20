47b96249acf1c8397a20f95b84b1adf23c8ec221
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-20 17:06:08 +0900

    2Create

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const a = 5;
 const b = 6;
-const myName = "konan";
+let myName = "konan";
+/*const는 불변, let은 변함, const를 주로쓴다*/
 
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log("hello " + myName);
+
+myName = "koyoi konan";
+console.log("kimozzi " + myName);
