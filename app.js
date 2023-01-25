3d17b4d3831d1913c2f5c7ef314bc2f320512f38
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-25 15:20:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-const a = 5;
-const b = 6;
-let myName = "konan";
-/*const는 불변, let은 변함, const를 주로쓴다*/
+function sayHello(nameOfPerson) {
+  console.log(nameOfPerson);
+}
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log("hello " + myName);
-
-myName = "koyoi konan";
-console.log("kimozzi " + myName);
+sayHello("konan");
