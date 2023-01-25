2014fbddd8a3725643c1327a195558ca770ffbd2
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-25 14:55:03 +0900

    1-Create

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-const a = 5;
-const b = 6;
-let myName = "konan";
-/*const는 불변, let은 변함, const를 주로쓴다*/
-
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log("hello " + myName);
-
-myName = "koyoi konan";
-console.log("kimozzi " + myName);
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
+sayHello();
+sayHello();
---
+++ k1.py
@@ -0,0 +1,6 @@
+#달력 출력!!
+import calendar
+yy=2023
+for x in range(12):
+    mm=x+1
+    print(calendar.month(yy,mm))
