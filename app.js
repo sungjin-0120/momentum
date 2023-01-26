9d9eebf1101c2ea4f5334c8936fec4f7f64cbbbd
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-26 17:56:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const age = prompt("How old are You?");
+const age = parseInt(prompt("How old are You?"));
 
-console.log(age, parseInt(age));
+if (isNaN(age)) {
+  console.log("please write a number");
+} else if (age < 18) {
+  console.log("Go home BABY");
+} else {
+  console.log("You can Drink");
+}
