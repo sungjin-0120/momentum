80ab15b8089c495f3d0655558abf1be99267df20
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-26 15:42:56 +0900

    3.Update

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-const iphone = {
-  ap: "A15",
-  battery: 3500,
-  price: 25550,
-};
-
-const day = ["mon", "tue"];
-console.log(day);
-day.push("wed");
-console.log(day);
-console.log(day[0]);
-console.log(iphone);
-iphone.battery = 2500;
-console.log(iphone);
-
-function hello(name, age) {
-  console.log(name + " The name is, years old" + age);
+const age = 12;
+function calAge(ageOfForeigner) {
+  return ageOfForeigner + 2;
 }
-hello("bond", 45);
+const krAge = calAge(age);
+console.log(krAge);
