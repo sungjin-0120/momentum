0bec9a3934e6a11b0629b4cd763e30d383c1d974
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-29 20:05:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const age = parseInt(prompt("How old are You?"));
+const naJeung = {
+  name: "나정",
+  boob: "75D",
+};
 
-if (isNaN(age) || age < 0) {
-  console.log("please write a real positive number");
-} else if (age < 18) {
-  console.log("Go home BABY");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink!!");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exersise");
-} else if (age === 100) {
-  console.log("You are wise!");
-} else if (age > 80) {
-  console.log("You can do whatever you want");
-}
+console.log(naJeung);
+
+naJeung.boob = "80F";
+console.log(naJeung);
