b123dd60b63463ac054aa7beefb500bc4db3f453
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-30 00:14:11 +0900

    Update k1.py

--- k1.py
+++ k1.py
@@ -1,13 +1,7 @@
-"""
 #달력 출력!!
 import calendar
-yy=2023
+x=int(input("어떤 년 보고 싶어?:\n"))
+yy=x
 for x in range(12):
     mm=x+1
     print(calendar.month(yy,mm))
-"""
-def say_hello(name, age):
-    return (f"Hello My name is {name} and {age} years old!")
-
-x_h= say_hello('konan',25)
-print(x_h)
