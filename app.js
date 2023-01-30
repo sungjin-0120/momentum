7d790f419495521d016730aac415de3305fb8f3a
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-30 17:53:36 +0900

    4.Update

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const title = document.getElementById("title");
+const title = document.querySelector("div.hello:first-child h1");
 
-title.innerText = "Got you!";
-console.log(title.id);
+console.dir(title);
+
+function handileTitleClick() {
+  title.style.color = "tomato";
+}
+function handleMouseEnter() {
+  title.innerText = "Mouse is here";
+}
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone";
+}
+
+title.addEventListener("click", handileTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
