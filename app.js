18ad2dab774458cc0791cfaec8d473e42f28ea63
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-31 16:28:22 +0900

    Update-app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handileTitleClick() {
-  if (h1.style.color === "blue") {
-    h1.style.color = "tomato ";
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    h1.style.color === "blue";
+    h1.className = clickedClass;
   }
 }
 
-h1.onclick = handileTitleClick;
+h1.addEventListener("click", handileTitleClick);
