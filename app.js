d1e73a9f9c2bed2d9d90c80a15b0eb3980e02d96
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-31 13:50:37 +0900

    5.Update

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handileTitleClick() {
-  title.style.color = "tomato";
+  h1.style.color = "tomato";
 }
 function handleMouseEnter() {
-  title.innerText = "Mouse is here";
+  h1.innerText = "Mouse is here";
 }
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone";
+  h1.innerText = "Mouse is gone";
+}
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy() {
+  alert("copier!!");
+}
+function handleWindowOffline() {
+  alert("sos no WIFI");
+}
+function handleWindowOnline() {
+  alert("all GOOD!!");
 }
 
-title.addEventListener("click", handileTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+h1.onclick = handileTitleClick;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.onmouseleave = handleMouseLeave;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
