6f6f53139f614777082f52e432f3833eb35cb95d
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-01-31 14:45:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handileTitleClick() {
-  h1.style.color = "tomato";
-}
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here";
-}
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone";
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy() {
-  alert("copier!!");
-}
-function handleWindowOffline() {
-  alert("sos no WIFI");
-}
-function handleWindowOnline() {
-  alert("all GOOD!!");
+  if (h1.style.color === "blue") {
+    h1.style.color = "tomato ";
+  } else {
+    h1.style.color === "blue";
+  }
 }
 
 h1.onclick = handileTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.onmouseleave = handleMouseLeave;
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
