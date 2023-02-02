1d8f332653df8906672d4cbd4057a9dce45d6480
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-02-02 15:53:28 +0900

    7.Update

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const loginForm = document.querySelector("#login-form ");
 const loginInput = document.querySelector("#login-form input");
-function onLoginSubmit() {}
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const userName = loginInput.value;
+  loginForm.classList.add("hidden");
+}
 
 loginForm.addEventListener("submit", onLoginSubmit);
