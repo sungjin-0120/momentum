dbdd55cc5c4affdd99649ce4411e046629704840
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-02-09 16:20:29 +0900

    8.Login-update

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const loginForm = document.querySelector("#login-form ");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const hidden = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const userName = loginInput.value;
-  loginForm.classList.add("hidden");
+  loginForm.classList.add(hidden);
+  const typedUserName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, typedUserName);
+  paintGreeting(typedUserName);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(userName) {
+  greeting.innerText = `Hello ${userName}`;
+  greeting.classList.remove(hidden);
+}
+
+const savedName = localStorage.getItem(USERNAME_KEY);
+if (savedName === null) {
+  loginForm.classList.remove(hidden);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedName);
+}
