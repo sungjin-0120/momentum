0012c20bf3e61a95854882b5aca9cdf6d2bd6cb7
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-02-15 16:06:27 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,20 +1,32 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
-
+const todos = [];
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
 function paindTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   todoList.appendChild(li);
-  
 }
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+  todos.push(newTodo);
   paindTodo(newTodo);
+  saveTodos();
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
