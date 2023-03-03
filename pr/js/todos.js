976a674a2c8fe49f0ca1c3e84a35f4e3b7c646b4
sungjin-0120 <73540530+sungjin-0120@users.noreply.github.com>
2023-03-03 15:14:40 +0900

    Update todos.js

--- pr/js/todos.js
+++ pr/js/todos.js
@@ -1,7 +1,7 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoLisst = document.getElementById("todo-list");
-const storage = [];
+let storage = [];
 
 function saveTodos() {
   localStorage.setItem("todos", JSON.stringify(storage));
@@ -11,19 +11,26 @@ function handleTodoSubmit(event) {
   event.preventDefault();
   const todoInputValue = todoInput.value;
   todoInput.value = "";
-  storage.push(todoInputValue);
-  writeTodo(todoInputValue);
+  const todoObj = {
+    text: todoInputValue,
+    id: Date.now(),
+  };
+  storage.push(todoObj);
+  writeTodo(todoObj);
   saveTodos();
 }
 function deleteTodo(event) {
   const lii = event.target.parentElement;
   lii.remove();
+  storage = storage.filter((todo) => todo.id !== parseInt(lii.id));
+  saveTodos();
 }
 
 function writeTodo(todoInputValue) {
   const li = document.createElement("li");
+  li.id = todoInputValue.id;
   const span = document.createElement("span");
-  span.innerText = todoInputValue;
+  span.innerText = todoInputValue.text;
   const button = document.createElement("button");
   button.innerText = "✖";
   button.addEventListener("click", deleteTodo);
@@ -38,6 +45,7 @@ const savedTodos = localStorage.getItem("todos");
 
 if (savedTodos != null) {
   const parsedTodos = JSON.parse(savedTodos);
+  storage = parsedTodos;
   parsedTodos.forEach(writeTodo);
   //foreach = for in py
   //[]에 저장된 녀석들을 사용하기위해 스트링을 어레이로 바꾸기위해
